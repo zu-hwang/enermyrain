fe37b5e4ce7a380b66baf9f07c7bf3d302a6ace0
zu-hwang <tropical.kitsch@gmail.com>
2020-02-06 18:50:57 +0900

    ss

--- enemy.js
+++ enemy.js
@@ -1,41 +1,55 @@
-import { Http2ServerResponse } from "http2";
+
+const ghost = document.querySelector('.ghost');
+// const hero = document.querySelector('.hero');
+let nowY;
+let nowX;
 
 
-// 고스트 메이커!
-class Enemy {
-  constructor() {
-    this.creater();
-  }
 
-  creater() {
-    const ghost = document.querySelector('.ghost');
-    let nowY;
-  }
-  rainGhost() {
-    let curY = parseInt(getComputedStyle(ghost).top);
-    // console.log(curY);s
-    nowY = ++curY;
-    if (nowY <= 600) {
-      ghost.style.top = nowY + 'px';
-      // checkPosition();
-    } else if (nowY > 600) {
-      ghost.parentNode.removeChild(ghost);
-    }
-    console.log();
-  }
 
-  checkPosition(params) {
-    // 고스트의 (X,Y) , 히어로 (X,Y) 좌표 위치가 같을때,
-    // 고스트 배경화면 좌표 변경, 현재위치 고정, 2초후에 시체 사라지기 , 킬 카운트 추가
-    // 만약 고스트가 배경화면 좌표까지 떨어지면, 게임 오버
-  }
-
+function removeGhost() {
+  ghost.parentNode.removeChild(ghost);
 }
 
+function deadGhost(gX, gY, hX, hY) {
+  console.log(gX, gY, hX, hY);
 
+  hY = parseInt(getComputedStyle(hero).wigth);
+  gY = parseInt(getComputedStyle(ghost).top) - 90;
+  console.log('>>>', gX, gY, hX, hY);
 
+  // if (hy < gy && gy < hY + 140) {
+  if (true) {
+    clearInterval(1);
+    console.log('인터벌지우고');// 지워질꺼야 
+    setTimeout(removeGhost, 1000);
+    console.log('고스트 시체 치웠다');// 지워질꺼야 
 
+  }
+}
+function rainGhost() {
+  let curY = parseInt(getComputedStyle(ghost).top);
+  // console.log(curY);s
+  nowY = ++curY;
+  nowX = parseInt(getComputedStyle(ghost).left);
+  if (nowY <= 600) {
+    ghost.style.top = nowY + 'px';
+    // checkPosition();
+  } else if (nowY > 600) {
+    removeGhost();
+  }
+  deadGhost(nowX, nowY);
 
-setInterval(new Enermy, 1000 / 60);
+  console.log();
+}
 
+function checkPosition(params) {
+  // 고스트의 (X,Y) , 히어로 (X,Y) 좌표 위치가 같을때,
+  // 고스트 배경화면 좌표 변경, 현재위치 고정, 2초후에 시체 사라지기 , 킬 카운트 추가
+  // 만약 고스트가 배경화면 좌표까지 떨어지면, 게임 오버
+}
 
+// const id = 
+setInterval(rainGhost, 1000 / 60);
+
+// console.log(id);
--- hero2.js
+++ hero2.js
@@ -13,21 +13,21 @@ class Hero {
 
 
 // 히어로 방향 > 왼쪽보는 함수
-function leftHeroSight() {
-  hero.classList.remove('front');
+function leftSight() {
+  // hero.classList.remove('front');
   hero.classList.remove('right');
   hero.classList.add('left');
 }
 
 // 히어로 방향 > 오른쪽보는 함수
-function rightHeroSight() {
-  hero.classList.remove('front');
+function rightSight() {
+  // hero.classList.remove('front');
   hero.classList.remove('left');
   hero.classList.add('right');
 }
 
 // 히어로 방향 > 정면보는 함수
-function frontHeroSight() {
+function frontSight() {
   hero.classList.remove('right');
   hero.classList.remove('left');
   hero.classList.add('front');
@@ -73,21 +73,22 @@ function handleKeyEvent(event) {
   // 키코드에 따른 함수 실행
   if (curKey === 37) {
     // console.log('왼쪽으로 갈꺼얍');
-    leftHeroSight(); // 왼쪽 보는 모습 CSS 적용
+    leftSight(); // 왼쪽 보는 모습 CSS 적용
     moveLeft();
-
-
+    window.addEventListener('keyup', frontSight); // 셋타입아웃으로 앞면보게 했는데 그럼 버퍼처럼 덜덜거림. 이벤트로 주는 것이 깔꾸미
 
   } else if (curKey === 39) {
     // console.log('오른쪼오오오오옥!');
-    rightHeroSight(); // 오른쪽 보는 모습 CSS 적용
+    rightSight(); // 오른쪽 보는 모습 CSS 적용
     moveRight();
-
-
+    // setTimeout((hero) => {
+    //   frontSight()
+    // }, 1000);
+    window.addEventListener('keyup', frontSight);
 
   } else if (curKey === 38 || curKey === 40) {
     // console.log('정면보기!');
-    frontHeroSight(); // 오른쪽 보는 모습 CSS 적용
+    frontSight(); // 오른쪽 보는 모습 CSS 적용
   }
 }
 
